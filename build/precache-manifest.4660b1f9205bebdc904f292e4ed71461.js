e6dbbe3fc3903b6d193e3ad8b628aaebecaeba86
dalisc <42921826+dalisc@users.noreply.github.com>
2019-07-29 00:38:54 +0700

    add build folder

---
+++ build/precache-manifest.4660b1f9205bebdc904f292e4ed71461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5a35988ad9109348e3303a52f29e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e682562c1d869d523be",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "80b29b906fc7dee7059f",
+    "url": "/static/css/main.89f7a351.chunk.css"
+  },
+  {
+    "revision": "6e682562c1d869d523be",
+    "url": "/static/js/2.f66d55b9.chunk.js"
+  },
+  {
+    "revision": "80b29b906fc7dee7059f",
+    "url": "/static/js/main.2734068f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c6891816d6ef9bfb6c23de136077be9",
+    "url": "/static/media/icon256.8c689181.png"
+  }
+]);
